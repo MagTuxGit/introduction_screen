ac07458f7d68a3c03fa6a402f50617e3eb8345d3
Andriy Trubchanin <magtuxgit@gmail.com>
2022-10-24 21:23:02 +0300

    collection fix

--- lib/src/helper.dart
+++ lib/src/helper.dart
@@ -3,10 +3,10 @@ extension CustomList<T> on List<T> {
     return isReverse ? reversed.toList() : this;
   }
 
-  T? elementAtOrNull(int index) {
-    try {
-      return this[index];
-    } catch (_) {}
-    return null;
-  }
+  // T? elementAtOrNull(int index) {
+  //   try {
+  //     return this[index];
+  //   } catch (_) {}
+  //   return null;
+  // }
 }
